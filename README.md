49bb0fa664f5b07a8cd4d8c8624f38d89cf70eea
akg <akg.bit@gmail.com>
2021-10-25 09:28:54 +0530

    Google Oauth using Springboot added

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # matrimony
+
+OAuth Client ID: 568462206103-vuocs6pkss56r5njhhr03rpv7mugoevb.apps.googleusercontent.com
+OAuth Client Secret: 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
---
+++ matrimony-server/src/main/java/com/jamiladeveloper/matrimonyserver/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.jamiladeveloper.matrimonyserver.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable().antMatcher("/**").authorizeRequests()
+                .antMatchers("/", "/index.html").authenticated()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login().permitAll()
+                .and().
+                logout().logoutSuccessUrl("/");
+    }
+}
